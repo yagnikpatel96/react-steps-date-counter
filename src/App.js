97bcbd8ffe95312fbd3d6d2b845e3ce32ba6d345
yagnik <yagnik.diyora@upayments.com>
2023-10-15 12:16:44 +0530

    reset function applied

--- src/App.js
+++ src/App.js
@@ -56,15 +56,20 @@ function Counter(){
 
   const date = new Date('Oct 7 2023');
   date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   return (<div>
     <div>
-      <button onClick={() => setStep((c) => c - 1 )}>-</button>
-        <span>Steps : {step}</span>
-      <button onClick={() => setStep((c) => c + 1 )}>+</button>
+      <input type="range" min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))} />
+      <span>Step: {step}</span>
     </div>
     <div>
       <button onClick={() => setCount((c) => c - 1 - step )}>-</button>
-        <span>Count : {count}</span>
+        <input type="text" value={count} onChange={(e) => setCount(Number(e.target.value))}/>
       <button onClick={() => setCount((c) => c + 1 + step )}>+</button>
     </div>
     <p>
@@ -73,5 +78,11 @@ function Counter(){
       <span>{count < 0 && `${Math.abs(count)} days ago, it was `}</span>
       <span>{date.toDateString()}</span>
     </p>
+    {count !== 0 && step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null
+    }
   </div>)
 }
