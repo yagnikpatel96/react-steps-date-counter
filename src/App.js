d88a7b02b7079d63ed70bc2ede72515544cddf02
yagnik <yagnik.diyora@upayments.com>
2023-10-29 11:29:11 +0530

    introducing Counter and Accordion modules (yagnik)

--- src/App.js
+++ src/App.js
@@ -4,28 +4,44 @@ const messages = [
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
-export default function App(){
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+export default function App() {
   return (
     <div className="App">
       <Steps />
       <Counter />
+      <Accordian data={faqs} />
     </div>
   );
 }
 
-
-function Steps(){
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setOpen] = useState(true);
   return (
     <>
-      <button className="close" onClick={()=> setOpen((is) => !is)}>&times;</button>
-      { isOpen && (
+      <button className="close" onClick={() => setOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
         <div className="steps">
           <div className="numbers">
-            <div className={step >= 1 ? "active" : "" }>1</div>
-            <div className={step >= 2 ? "active" : "" }>2</div>
-            <div className={step >= 3 ? "active" : "" }>3</div>
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
           <p className="message">
@@ -33,28 +49,42 @@ function Steps(){
           </p>
 
           <div className="buttons">
-            <button style={{backgroundColor: '#7950f1', color: '#fff'}} onClick={handlePrevious}>Previous</button>
-            <button style={{backgroundColor: '#7950f1', color: '#fff'}} onClick={handleNext}>Next</button>
+            <button
+              style={{ backgroundColor: "#7950f1", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f1", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
-      )}  
+      )}
     </>
-  )
+  );
 
-  function handlePrevious(){
-    if(step > 1) { setStep((s) => s - 1); }
+  function handlePrevious() {
+    if (step > 1) {
+      setStep((s) => s - 1);
+    }
   }
 
-  function handleNext(){
-    if(step < 3) { setStep((s) => s + 1); }
+  function handleNext() {
+    if (step < 3) {
+      setStep((s) => s + 1);
+    }
   }
 }
 
-function Counter(){
+function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  const date = new Date('Oct 7 2023');
+  const date = new Date("Oct 7 2023");
   date.setDate(date.getDate() + count);
 
   function handleReset() {
@@ -62,27 +92,63 @@ function Counter(){
     setStep(1);
   }
 
-  return (<div>
+  return (
     <div>
-      <input type="range" min="0" max="10" value={step} onChange={(e) => setStep(Number(e.target.value))} />
-      <span>Step: {step}</span>
-    </div>
-    <div>
-      <button onClick={() => setCount((c) => c - 1 - step )}>-</button>
-        <input type="text" value={count} onChange={(e) => setCount(Number(e.target.value))}/>
-      <button onClick={() => setCount((c) => c + 1 + step )}>+</button>
-    </div>
-    <p>
-      <span>{count === 0 && 'Today is '}</span>
-      <span>{count > 0 && `${count} days from Today is `}</span>
-      <span>{count < 0 && `${Math.abs(count)} days ago, it was `}</span>
-      <span>{date.toDateString()}</span>
-    </p>
-    {count !== 0 && step !== 1 ? (
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span>Step: {step}</span>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - 1 - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + 1 + step)}>+</button>
+      </div>
+      <p>
+        <span>{count === 0 && "Today is "}</span>
+        <span>{count > 0 && `${count} days from Today is `}</span>
+        <span>{count < 0 && `${Math.abs(count)} days ago, it was `}</span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 && step !== 1 ? (
         <div>
           <button onClick={handleReset}>Reset</button>
         </div>
-      ) : null
-    }
-  </div>)
+      ) : null}
+    </div>
+  );
+}
+
+function Accordian({ data }) {
+  return (
+    <div className="accordion">
+      {data.map((el, i) => (
+        <AccordianItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
+    </div>
+  );
+}
+
+function AccordianItem({ title, text, num }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
+      <p className="title">{title}</p>
+      <p className="icon">-</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
 }
